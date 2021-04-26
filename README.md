e4e3001d3ffdf0b15ba65afd6da418d48235e285
ahmetpergamum <ahmet.pergamum@gmail.com>
2021-04-26 05:56:23 +0300

    world map chart added

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # ***Streamlit EDA Application***
 
+- This is a toy application to test the capabilities of Streamlit framework.
+
+- Check it out the deployed app on [heroku.](https://streamlit-eda.herokuapp.com/)
+
+### How to run the app?
+
+- Clone the repository.
+```
+git clone https://github.com/ahmetpergamum/streamlitEDApp.git && cd streamlitEDApp
+```
+
 - Create a virtual environment.
 ```
 virtualenv  venv
--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('./data/suicide_clean.csv')
-
-# st.set_page_config(layout='wide')
-# st.title('Exploratory Data Analysis(EDA) for Suicide Rates')
+df = pd.read_csv('./data/suicides_clean.csv')
+st.set_page_config(page_title='Suicide Rates',layout='wide')
 st.write('#### Exploratory Data Analysis(EDA) for Suicide Rates')
-
-# st.markdown('How are **the suicide rates** affected?')
 
 continuous = [col for col in df.columns if df[col].dtype!='object']
 categoric = [col for col in df.columns if df[col].dtype=='object']
 
 sidebar_select_ops = st.sidebar.selectbox(
     "Select the type of anaysis.",
-    ("Dataframe", "Univariate-continuous", "Univariate-categoric")
+    ("Dataframe", "Univariate-continuous", "Univariate-categoric", 'World map'),
+    index=3
 )
-
 
 if sidebar_select_ops=='Dataframe':
     st.write('Dataframe exploration')
@@ -30,7 +26,7 @@ if sidebar_select_ops=='Dataframe':
 elif sidebar_select_ops=='Univariate-continuous': 
     st.write('Univariate Analysis of Continuous Variables')
     op_cont = st.sidebar.selectbox('Select the variable.', (continuous))
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(20,6))
     ax = sns.distplot(df[op_cont], kde=False)
     ax.set(title='Histogram of ' + op_cont)
     st.pyplot(fig)
@@ -43,6 +39,28 @@ elif sidebar_select_ops=='Univariate-categoric':
     ax.tick_params(axis="x", rotation=90)
     ax.set(title='Countplot of ' + op_cat)
     st.pyplot(fig)
+elif sidebar_select_ops=='World map': 
+    df_agg = df.groupby(['country','year']).agg(suicide_sum=('suicides_no','sum'), 
+                                       suicide_100k_pop=('suicides_100k_pop','sum'),
+                                       population=('population','sum'),
+                                       region=('region','first'), sub_region=('sub_region','first'),
+                                       gdp=('gdp','first'),
+                                       gdp_per_capita=('gdp_per_capita','first'),
+                                       iso_alpha=('iso_alpha','first'),
+                                      )
+    df_agg = df_agg.reset_index()
+    df_agg = df_agg.sort_values(by=['year','country'])
+    years = list(df_agg.year.unique())
+    op_year = st.sidebar.selectbox('Select the year.', (years),index=years.index(2015) )
+    op_color = st.sidebar.selectbox('Select the color.', ('gdp_per_capita','region','sub_region','gdp'))
+    op_size = st.sidebar.selectbox('Select the size.', ('suicide_100k_pop','suicide_sum'))
+    st.write('World map for year '+str(op_year))
 
-
-# st.balloons()
+    fig = px.scatter_geo(df_agg[df_agg.year==op_year], locations="iso_alpha", color=op_color,
+                         hover_name="country", size=op_size,
+                         hover_data={'suicide_sum': True,"suicide_100k_pop": ":.2f",
+                             "population": True, "gdp_per_capita": True, "iso_alpha": False},
+                         #projection="orthographic"
+                         )
+    fig.update_layout( width=800, height=800, hoverlabel_align = 'right' )
+    st.plotly_chart(fig, use_container_width=True)
